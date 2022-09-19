dadd91ec154811c4817dcb1773f7466fc9be6804
bukeme <basseyukemme@gmail.com>
2022-09-19 09:27:15 +0100

    final project

--- app.js
+++ app.js
@@ -16,14 +16,53 @@ const moveToImg = (list, currentImg, targetImg) => {
     targetImg.classList.add('current--img')
 }
 
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current--img')
+    targetDot.classList.add('current--img')
+}
+
+const hideShowArrows = (targetIndex) => {
+    if (targetIndex === 0) {
+        prevBtn.classList.add('hidden')
+    } else if (targetIndex === imgs.length - 1) {
+        nextBtn.classList.add('hidden')
+    } else {
+        prevBtn.classList.remove('hidden')
+        nextBtn.classList.remove('hidden')
+    }
+}
+
 nextBtn.addEventListener('click', (e) => {
     const currentImg = list.querySelector('.current--img')
     const nextImg = currentImg.nextElementSibling
     moveToImg(list, currentImg, nextImg)
+    const currentDot = carouselNav.querySelector('.current--img')
+    const nextDot = currentDot.nextElementSibling
+    const nextIndex = imgs.indexOf(nextImg)
+    updateDots(currentDot, nextDot)
+    hideShowArrows(nextIndex)
 })
 
 prevBtn.addEventListener('click', (e) => {
     const currentImg = list.querySelector('.current--img')
     const prevImg = currentImg.previousElementSibling
     moveToImg(list, currentImg, prevImg)
+    const currentDot = carouselNav.querySelector('.current--img')
+    const prevDot = currentDot.previousElementSibling
+    const prevIndex = imgs.indexOf(prevImg)
+    updateDots(currentDot, prevDot)
+    hideShowArrows(prevIndex)
+})
+
+carouselNav.addEventListener('click', (e) => {
+    const targetDot = e.target.closest('button')
+    if (!targetDot) return
+    const currentImg = list.querySelector('.current--img')
+    const currentDot = carouselNav.querySelector('.current--img')
+    const targetIndex = dots.findIndex((dot) => dot === targetDot)
+    const targetImg = imgs[targetIndex]
+    moveToImg(list, currentImg, targetImg)
+    updateDots(currentDot, targetDot)
+    hideShowArrows(targetIndex)
+
 })
